72d4e40c1fc88320c16022af5ef851332406c4c5
Kevin Chen <kevnchn@gmail.com>
2020-01-18 15:46:31 -0500

    Add custom webpack config

---
+++ elmapp.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  homepage: "https://kevn-chn.github.io/sc-crossfade-pwa",
+  configureWebpack: (config, env) => {
+    // Manipulate the config object and return it.
+    return config;
+  }
+}
