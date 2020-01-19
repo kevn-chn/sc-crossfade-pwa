20132e423e54b760659f3a9cf53a7ca233ac210c
Kevin Chen <kevnchn@gmail.com>
2020-01-19 14:15:06 -0500

    Add icons and turn playlists into accordions

--- src/Main.elm
+++ src/Main.elm
@@ -2,12 +2,16 @@ module Main exposing (..)
 
 import Api exposing (Flags, url)
 import Browser
+import Dict exposing (Dict)
+import FeatherIcons as Icons
 import Html exposing (..)
-import Html.Attributes exposing (class, href, rel, src, target)
+import Html.Attributes exposing (class, href, rel, src, target, title)
+import Html.Events exposing (onClick, stopPropagationOn)
 import Http
 import HttpBuilder exposing (RequestBuilder, withBody, withExpect)
 import Json.Decode as Decode exposing (Decoder, succeed)
 import Json.Decode.Pipeline as Pipeline exposing (optional, required)
+import Svg.Attributes
 import Task exposing (Task)
 
 
@@ -32,6 +36,7 @@ type alias TrackInfo =
 
 type alias PlaylistInfo =
     { title : String
+    , id : Int
     , permalink_url : String
     , user : User
     , tracks : List TrackInfo
@@ -39,8 +44,24 @@ type alias PlaylistInfo =
     }
 
 
+type alias PlaylistState =
+    { isOpen : Bool }
+
+
+type alias PlayerState =
+    { isPlaying : Bool
+    , currentTrackId : Int
+    }
+
+
 type alias PlaylistsCollection =
     List PlaylistInfo
+
+
+type alias UIState =
+    { playlistsById : Dict Int PlaylistState
+    , player : PlayerState
+    }
 
 
 
@@ -51,6 +72,7 @@ type alias Model =
     { flags : Flags
     , playlists : List PlaylistInfo
     , user : User
+    , ui : UIState
     }
 
 
@@ -58,14 +80,34 @@ initialModel : Flags -> Model
 initialModel flags =
     { flags = flags
     , playlists = []
-    , user =
-        { avatar_url = ""
-        , id = 0
-        , kind = ""
-        , last_modified = ""
-        , permalink_url = ""
-        , username = ""
+    , ui =
+        { player = defaultPlayerState
+        , playlistsById = Dict.empty
         }
+    , user = defaultUser
+    }
+
+
+defaultPlayerState : PlayerState
+defaultPlayerState =
+    { isPlaying = False
+    , currentTrackId = 0
+    }
+
+
+defaultPlaylistState : PlaylistState
+defaultPlaylistState =
+    { isOpen = False }
+
+
+defaultUser : User
+defaultUser =
+    { avatar_url = ""
+    , id = 0
+    , kind = ""
+    , last_modified = ""
+    , permalink_url = ""
+    , username = ""
     }
 
 
@@ -78,6 +120,7 @@ playlistDecoder : Decoder PlaylistInfo
 playlistDecoder =
     succeed PlaylistInfo
         |> required "title" Decode.string
+        |> required "id" Decode.int
         |> required "permalink_url" Decode.string
         |> required "user" userDecoder
         |> required "tracks" (Decode.list trackDecoder)
@@ -139,6 +182,7 @@ type Msg
     = NoOp
     | GotUserInfo (Result Http.Error User)
     | GotPlaylistsCollection (Result Http.Error PlaylistsCollection)
+    | TogglePlaylistAccordion Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -151,10 +195,41 @@ update msg model =
             ( model, Cmd.none )
 
         GotPlaylistsCollection (Ok collection) ->
-            ( { model | playlists = collection }, Cmd.none )
+            let
+                { ui } =
+                    model
+
+                playlistsById =
+                    Dict.fromList (List.map (\list -> ( list.id, { isOpen = False } )) collection)
+
+                uiState =
+                    { ui | playlistsById = playlistsById }
+            in
+            ( { model | playlists = collection, ui = uiState }, Cmd.none )
 
         GotPlaylistsCollection (Err _) ->
             ( model, Cmd.none )
+
+        TogglePlaylistAccordion id ->
+            let
+                { ui } =
+                    model
+
+                playlistState =
+                    case Dict.get id ui.playlistsById of
+                        Just state ->
+                            state
+
+                        Nothing ->
+                            { isOpen = False }
+
+                playlistsById =
+                    Dict.insert id { playlistState | isOpen = not playlistState.isOpen } ui.playlistsById
+
+                uiState =
+                    { ui | playlistsById = playlistsById }
+            in
+            ( { model | ui = uiState }, Cmd.none )
 
         NoOp ->
             ( model, Cmd.none )
@@ -164,36 +239,62 @@ update msg model =
 ---- VIEW ----
 
 
+onClickStopPropagation : msg -> Attribute msg
+onClickStopPropagation msg =
+    stopPropagationOn "click"
+        (Decode.succeed ( msg, True ))
+
+
 view : Model -> Html Msg
-view model =
+view { playlists, ui, user } =
     div [ class "max-w-2xl m-6 md:mx-auto" ]
         [ h1 [ class "mb-4" ]
             [ a
                 [ class "hover:underline"
-                , href model.user.permalink_url
+                , href user.permalink_url
                 , rel "noopener noreferrer"
                 , target "_blank"
                 ]
-                [ text model.user.username ]
+                [ text user.username ]
             ]
-        , viewPlaylists model.playlists
+        , viewPlaylists playlists ui.playlistsById
         ]
 
 
-viewPlaylists : List PlaylistInfo -> Html Msg
-viewPlaylists playlists =
+viewPlaylists : List PlaylistInfo -> Dict Int PlaylistState -> Html Msg
+viewPlaylists playlists uiPlaylistsById =
     ul []
         (List.map
             (\playlist ->
-                li [ class "border rounded mb-6 p-4 shadow-lg" ]
-                    [ a
-                        [ class "text-xl font-bold hover:underline"
-                        , href playlist.permalink_url
-                        , rel "noopener noreferrer"
-                        , target "_blank"
+                let
+                    isOpen =
+                        case Dict.get playlist.id uiPlaylistsById of
+                            Just state ->
+                                state.isOpen
+
+                            Nothing ->
+                                False
+                in
+                li [ class "border rounded mb-6 shadow-lg" ]
+                    [ button
+                        [ class "text-xl font-bold flex justify-between items-center p-4 w-full"
+                        , onClick (TogglePlaylistAccordion playlist.id)
                         ]
-                        [ text playlist.title ]
-                    , ul [ class "mt-4" ] (List.map viewTrack playlist.tracks)
+                        [ text playlist.title
+                        , a
+                            [ href playlist.permalink_url
+                            , rel "noopener noreferrer"
+                            , target "_blank"
+                            , title "Open playlist in Soundcloud"
+                            , onClickStopPropagation NoOp
+                            ]
+                            [ Icons.toHtml [] Icons.externalLink ]
+                        ]
+                    , if isOpen then
+                        ul [ class "px-4 pb-4" ] (List.map viewTrack playlist.tracks)
+
+                      else
+                        text ""
                     ]
             )
             playlists
@@ -202,17 +303,20 @@ viewPlaylists playlists =
 
 viewTrack : TrackInfo -> Html Msg
 viewTrack track =
-    li []
-        [ a
-            [ class "hover:underline"
-            , href track.permalink_url
-            , rel "noopener noreferrer"
-            , target "_blank"
-            ]
+    li [ class "flex justify-between items-center py-1" ]
+        [ p []
             [ span [] [ text track.user.username ]
             , text " - "
             , span [] [ text track.title ]
             ]
+        , a
+            [ class "hover:underline"
+            , href track.permalink_url
+            , rel "noopener noreferrer"
+            , target "_blank"
+            , title "Open track in Soundcloud"
+            ]
+            [ Icons.toHtml [ Svg.Attributes.class "p-1" ] Icons.externalLink ]
         ]
 
 
